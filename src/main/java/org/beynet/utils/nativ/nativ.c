2349fcd9b25f79a315aba779962c07f94feef421
Yannick Beynet <beynet@fresnes.info>
2009-04-09 19:15:53 +0200

    add compatibility with old libc

--- src/main/java/org/beynet/utils/nativ/nativ.c
+++ src/main/java/org/beynet/utils/nativ/nativ.c
@@ -4,17 +4,35 @@
 
 #include <jni.h>
 #include <stdio.h>
- #include <sys/inotify.h>
+
  
- #include <sys/types.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
- #include <sys/ioctl.h>
+#include <sys/ioctl.h>
 
 #include "nativ.h"
 
+#ifdef LIBC_NO_INOTIFY
+#include <sys/syscall.h>
+#include <linux/inotify.h>
+static int inotify_init (void)
+{
+	return syscall (__NR_inotify_init);
+}
+static int inotify_add_watch (int fd, const char *name, __u32 mask)
+{
+	return syscall (__NR_inotify_add_watch, fd, name, mask);
+}
 
+static int inotify_rm_watch (int fd, __u32 wd)
+{
+	return syscall (__NR_inotify_rm_watch, fd, wd);
+}
 
+#else
+#include <sys/inotify.h>
+#endif
 
 /*
  * Class:     Fd
