6177727036bad0418de0742dc4efe2aef75b9c16
Yannick Beynet <beynet@fresnes.info>
2009-10-27 19:52:31 +0100

    remove old comments

--- src/main/java/org/beynet/utils/messages/api/MessageQueueFactory.java
+++ src/main/java/org/beynet/utils/messages/api/MessageQueueFactory.java
@@ -10,23 +10,5 @@ package org.beynet.utils.messages.api;
  */
 public class MessageQueueFactory {
 	
-	/**
-	 * Construct a new MessageQueue with a DataBaseAccessor
-	 * @param queueName
-	 * @param accessor
-	 * @return
-	 * @throws UtilsException
-	 */
-	/*public static MessageQueue makeQueue(String queueName,RequestManager manager,DataBaseAccessor accessor) throws UtilsException {
-		// first create sql table (if needed)
-		manager.createTable(MessageQueueBean.class);
-		manager.createTable(MessageQueueConsumersBean.class);
-		return(new MessageQueueAdmin(new MessageQueueImpl(queueName,accessor)));
-	}*/
 	
-	/*public static MessageQueueSession createSession(RequestManager manager,MessageQueue queue,boolean transacted) {
-		MessageQueueSession session = new MessageQueueSessionImpl();
-		session.initEmptySession(manager, queue, transacted);
-		return(session);
-	}*/
 }
