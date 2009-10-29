727230a8c1a4665e8df727744926081906f62f0f
Yannick Beynet <beynet@fresnes.info>
2009-10-29 13:54:42 +0100

    change install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,1 @@
-mvn clean && mvn $@ install && ""cp -f target/webtools-1.0-SNAPSHOT.jar /opt/java/tomcat/lib
+mvn clean && mvn $@ install && ""cp -f target/webtools-1.4-SNAPSHOT.jar /opt/java/tomcat/lib
