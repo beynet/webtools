0e6ec8d08cb567e3a4703414566f789eeb92ad6a
Yannick Beynet <beynet@fresnes.info>
2015-07-14 16:57:58 +0200

    new method help from command line

--- src/main/java/org/beynet/utils/admin/commandline/CommandLineOptionsAnalyzer.java
+++ src/main/java/org/beynet/utils/admin/commandline/CommandLineOptionsAnalyzer.java
@@ -2,9 +2,12 @@ package org.beynet.utils.admin.commandline;
 
 import java.io.PrintStream;
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * class handling all the options found in a command line
@@ -12,6 +15,33 @@ import java.util.Map;
  *
  */
 public class CommandLineOptionsAnalyzer {
+
+    private static final Pattern TOKEN = Pattern.compile("^(\"[^\"]*\"|[^\"\\s]*)\\s*.*");
+
+    /**
+     * parse command line into tokens, tokens contained between " are grouped
+     * @param commandLine
+     * @return
+     */
+    public static List<String> parseCommandLine(String commandLine) {
+        List<String> result = new ArrayList<>();
+        Matcher matcher = TOKEN.matcher(commandLine);
+        while (matcher.matches()) {
+            String match = matcher.group(1);
+            String arg = match;
+            if (arg.startsWith("\"") && arg.endsWith("\"") && arg.length()>=2) {
+                arg = arg.substring(1, arg.length() - 1);
+            }
+            result.add(arg);
+            if (commandLine.length()==match.length()) break;
+            commandLine=commandLine.substring(match.length());
+            commandLine = commandLine.trim();
+            matcher=TOKEN.matcher(commandLine);
+        }
+        return result;
+    }
+
+
     /**
      * @param options
      * @throws IllegalArgumentException
--- src/test/java/org/beynet/utils/admin/commandline/CheckCommandLine.java
+++ src/test/java/org/beynet/utils/admin/commandline/CheckCommandLine.java
@@ -4,6 +4,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
@@ -30,14 +33,14 @@ public class CheckCommandLine {
         Option opt5 = new IntegerOption("--opt5", "this is the 5eme expected option",true);
         CommandLineOptionsAnalyzer cmd = new CommandLineOptionsAnalyzer(Arrays.asList(opt1,opt2,opt3,opt4,opt5));
         cmd.printHelp(System.err);
-        String [] line = {"--opt1","val1","--opt4","--opt5","1234","--opt2","val2"};
+        String [] line = {"--opt1","val1","--opt4","--opt5","1234","--opt2","val 2"};
         cmd.analyseCommandLine(line);
         
         assertThat(Boolean.valueOf(opt1.isOptionFound()), is(Boolean.TRUE));
         assertThat(opt1.getValue(), is((Object)"val1"));
         
         assertThat(Boolean.valueOf(opt2.isOptionFound()), is(Boolean.TRUE));
-        assertThat(opt2.getValue(), is((Object)"val2"));
+        assertThat(opt2.getValue(), is((Object)"val 2"));
         
         assertThat(Boolean.valueOf(opt3.isOptionFound()), is(Boolean.FALSE));
         
@@ -64,5 +67,17 @@ public class CheckCommandLine {
         assertThat(opt5.getValue(), is((Object)new Integer(1977)));
         
         
+    }
+
+    @Test
+    public void commandLine() {
+        String line = "machin \"essai coucou\" ok --teruc \"toto coucou\" ";
+        final List<String> strings = CommandLineOptionsAnalyzer.parseCommandLine(line);
+        assertThat(strings.size(),is(5));
+        assertThat(strings.get(0),is("machin"));
+        assertThat(strings.get(1),is("essai coucou"));
+        assertThat(strings.get(2),is("ok"));
+        assertThat(strings.get(3),is("--teruc"));
+        assertThat(strings.get(4),is("toto coucou"));
     }
 }
